5cd759810ffea9cd6abdcac414f93f77b21565b8
Nipi <156461231+Nipipo@users.noreply.github.com>
2025-01-11 21:48:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
-const io = require('socket.io')(server);
-let users = []; 
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files
+app.use(express.static('public'));
+
+// Setup Socket.io
 io.on('connection', (socket) => {
     console.log('A user connected');
-
+    
     socket.on('userJoined', (user) => {
-        users.push(user);
-        io.emit('userListUpdate', users); 
+        console.log(`${user.username} has joined`);
+        io.emit('userListUpdate', users); // Emit user list to all clients
     });
 
-    socket.on('placeBet', ({ username, betSide, amount }) => {
-        io.emit('bettingUpdate', { yesBet: 0, noBet: 0 });
+    socket.on('placeBet', (data) => {
+        console.log(`${data.username} placed a bet on ${data.betSide} with ${data.amount} coins.`);
+        // Update bet stats here
     });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected');
-        users = users.filter(user => user.socketId !== socket.id);
-        io.emit('userListUpdate', users); 
     });
+});
+
+// Start the server
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running...');
 });
